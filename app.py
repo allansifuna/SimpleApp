655fc74eda4c85fab467acbebac74a24ee1d1ee4
Allan Sifuna <allansifuna324@gmail.com>
2026-08-29 20:34:13 +0300

    feat: add ansible playbooks

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import os
 from flask import Flask, jsonify
 
 app = Flask(__name__)
-app.config["SERVICE_NAME"] = os.getenv("SERVICE_NAME", "simple-app")
+app.config["ENVIRONMENT"] = os.getenv("ENVIRONMENT", "dev")
+app.config["SERVICE_NAME"] = os.getenv("SERVICE_NAME", "rewards")
 app.config["COMMIT"] = os.getenv("COMMIT", None)
-app.config["REGION"] = os.getenv("REGION", "eu-west-1")
+app.config["REGION"] = os.getenv("REGION", "eu-west-2")
 
 
 @app.get("/healthz")
