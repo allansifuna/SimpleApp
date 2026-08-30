6410a3187865afc28a8fa60d1bfaf1f3ed25dedc
Allan Sifuna <allansifuna324@gmail.com>
2026-08-30 12:49:34 +0300

    chore: add a secret to trigger CI

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 app.config["ENVIRONMENT"] = os.getenv("ENVIRONMENT", "dev")
+app.config["APP_SECRET"] = os.getenv("APP_SECRET", None)
 app.config["SERVICE_NAME"] = os.getenv("SERVICE_NAME", "rewards")
 app.config["COMMIT"] = os.getenv("COMMIT", None)
 app.config["REGION"] = os.getenv("REGION", "eu-west-2")
@@ -14,6 +15,7 @@ def healthz():
         service=app.config["SERVICE_NAME"],
         status="ok",
         commit=app.config["COMMIT"],
+        app_secret=app.config["APP_SECRET"],
         region=app.config["REGION"],
     )
 
